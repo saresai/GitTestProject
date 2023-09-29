4250d6fafbc52d32ea4c0febace74a49c5d1d758
Sarah Rebecca <saresai.art@gmail.com>
2023-09-29 14:29:17 -0700

    Added new profile domain object

---
+++ src/com/coderscampus/domain/Profile.java
@@ -0,0 +1,27 @@
+package com.coderscampus.domain;
+
+public class Profile {
+	private Integer id;
+	private String name;
+	private String url;
+
+	public Integer getID() {
+		return id;
+	}
+	public void setID(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	pubilc String getURL() {
+		return url;
+	}
+	public void setURL(String url) {
+		this.url = url;
+	}
+}
